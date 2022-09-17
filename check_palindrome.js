590623f7733afe77e48495acc297b07c251393b2
hemensan mahilange <hmahilange9802@gmail.com>
2022-09-18 00:30:31 +0530

    check Palindrome or not

---
+++ check_palindrome.js
@@ -0,0 +1,12 @@
+// check Pal or Nor ?
+
+let str="kanak";
+let bag=0;
+for(let i=str.length-1;i>=0; i++){
+  bag+=str[i];
+}
+if(bag=str){
+  console.log(palindrone);
+}else{
+  console.log(Not a palindrone);
+}
